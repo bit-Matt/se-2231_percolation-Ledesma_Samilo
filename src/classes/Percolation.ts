a0149b1715e5f6691238b7d13997bf3d9cf9bf0b
bit-Matt <matthew.ledesma-22@cpu.edu.ph>
2024-01-23 11:04:23 +0800

    first commit

---
+++ src/classes/Percolation.ts
@@ -0,0 +1,31 @@
+class Percolation {
+  // creates n-by-n grid, with all sites initially blocked
+  constructor(n: number) {
+    return
+  }
+
+  // opens the site (row, col) if it is not open already
+  open(row: number, col: number) {
+    return
+  }
+
+  // is the site (row, col) open?
+  isOpen(row: number, col: number): boolean {
+    return true
+  }
+
+  // is the site (row, col) full?
+  isFull(row: number, col: number): boolean {
+    return true
+  }
+  
+  // returns the number of open sites
+  numberOfOpenSites(): number {
+    return 0
+  }
+
+  // does the system percolate?
+  percolates(): boolean {
+    return true
+  }
+}
---
+++ src/classes/PercolationStats.ts
@@ -0,0 +1,27 @@
+class PercolationStats {
+  // perform independent trials on an n-by-n grid
+  constructor(n: number, trials: number) {
+    return
+  }
+
+  // sample mean of percolation threshold
+  mean(): number {
+    return 0.0
+  }
+
+  // sample standard deviation of percolation threshold
+  stddev(): number {
+    return 0.0
+  }
+
+  // low endpoint of 95% confidence interval
+  confidenceLo(): number {
+    return 0.0
+  }
+  
+  // high endpoint of 95% confidence interval
+  confidenceHi(): number {
+    return 0.0
+  }
+}
+ 
