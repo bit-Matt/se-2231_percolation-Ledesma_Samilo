d8952c40964834d1021af82cc41c0c7786b5db52
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-28 18:08:17 +0800

    Finished(?) Perlocation Class + Index updates

--- src/index.ts
+++ src/index.ts
@@ -4,42 +4,56 @@ import Percolation from './classes/Percolation';
 //purely for display lng gd 
 function printGrid(percolation: Percolation) {
   for (let i = 0; i < percolation.rows; i++) {
-    console.log(percolation.grid[i].join(' '));
+     console.log(percolation.grid[i].join(' '));
   }
-}
+ }
 
-function main() {
+ function main() {
   const n = readlineSync.questionInt('Enter the size of the grid (n): ');
   const percolation = new Percolation(n);
-
+  let numOpenSites = 0;
+ 
   while (true) {
-    console.log('\nOptions:');
-    console.log('1. View current grid');
-    console.log('2. Open a random site');
-    console.log('3. Exit');
-
-
-    const choice = readlineSync.question('Enter your choice: ');
-
-    switch (parseInt(choice)) {
-      case 1:
-        printGrid(percolation);
-        break;
-
-      case 2:
-        let nowOpen = percolation.open();
-        console.log('Opening a random site.');
-        console.log(nowOpen);
-        break;
-      
-      case 3:
-        console.log('Exiting program.');
-        process.exit(0);
-
-      default:
-        console.log('Invalid choice. Please enter a valid option.');
-    }
+     console.log('\nOptions:');
+     console.log('1. View current grid');
+     console.log('2. Open a random site');
+     console.log('3. Open sites until the system percolates');
+     console.log('4. Check if the system percolates');
+     console.log('5. Exit');
+ 
+     const choice = readlineSync.question('Enter your choice: ');
+ 
+     switch (parseInt(choice)) {
+       case 1:
+         printGrid(percolation);
+         break;
+ 
+       case 2:
+         percolation.openRandomSite();
+         numOpenSites++;
+         console.log('Opening a random site.');
+         break;
+ 
+       case 3:
+         while (!percolation.percolates()) {
+            percolation.openRandomSite();
+            numOpenSites++;
+         }
+         console.log(`System percolated after ${numOpenSites} attempts.`);
+         break;
+ 
+       case 4:
+         console.log('Does the system percolate?', percolation.percolates());
+         break;
+ 
+       case 5:
+         console.log('Exiting program.');
+         process.exit(0);
+ 
+       default:
+         console.log('Invalid choice. Please enter a valid option.');
+     }
   }
-}
+ } 
 
 main();
